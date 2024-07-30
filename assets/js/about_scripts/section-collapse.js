70f3cb1ded5734360eb1105d4d8ac5daa1f4759d
rhysjo <roegpc@gmail.com>
2024-07-30 13:14:33 +0100

    Portfolio
     - Added info box hover on bootstrap logo for each page
     - Added styles for above change
     - Started adding content sections to about page (made them collapsible)
     - Had to force enable scrollbar on every page to stop viewport adjusting slightly on about, triggered my OCD. Can't find a better way to fix this just yet

---
+++ assets/js/about_scripts/section-collapse.js
@@ -0,0 +1,18 @@
+function aboutContentCollapse(){
+        const buttons = document.querySelectorAll('.collapsible-btn');
+        const contents = document.querySelectorAll('.collapsible-content');
+
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', function() {
+                const content = contents[index];
+                if (content.style.maxHeight) {
+                    content.style.maxHeight = null;
+                    content.style.padding = "0 0";
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            });
+        });
+}
+
+aboutContentCollapse();
